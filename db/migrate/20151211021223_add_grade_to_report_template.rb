41e5bd85d13a6963fb74e2ff73d96c15cbc5ed2a
ColinDao <quyetdc.uet@gmail.com>
2015-12-12 14:14:01 +0700

    fix migration on grade

--- db/migrate/20151211021223_add_grade_to_report_template.rb
+++ db/migrate/20151211021223_add_grade_to_report_template.rb
@@ -1,6 +1,7 @@
 class AddGradeToReportTemplate < ActiveRecord::Migration
   def up
     add_column :grades, :report_template_id, :integer
+    Grade.where(report_template_id: nil).update_all(report_template_id: ReportTemplate.all.first.id)
   end
 
   def down
