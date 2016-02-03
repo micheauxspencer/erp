7395218040e0c36ad054c09d3dc9d21f8f93c0ab
ColinDao <quyetdc.uet@gmail.com>
2016-02-03 10:48:44 +0700

    fix bug select grade and remove column create

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,6 @@ module ApplicationHelper
     title ||= column.titleize
     css_class = column == sort_column ? "current #{sort_direction}" : nil
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-    link_to title, {:sort => column, :direction => direction}, {:class => css_class}
+    link_to title, {grade_id: params[:grade_id], :sort => column, :direction => direction}, {:class => css_class}
   end
 end
